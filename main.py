03977ff83bee9b283f2148e49ef1fe625392d4bc
DaniilSmirnov <littlebigcrafting@gmail.com>
2018-10-13 20:29:11 +0300

    начало

--- main.py
+++ main.py
@@ -8,19 +8,57 @@ DISPLAY = (WIN_WIDTH, WIN_HEIGHT)  # Группируем ширину и выс
 BACKGROUND_COLOR = "#004400"
 
 
+class Country:
+
+    units = []
+    provinces = []
+    color = {}
+
+    def create(self, id, stability, units, provinces, symbols, color):
+        self.id = id
+        self.stability = stability
+        self.units = units
+        self.provinces = provinces
+        self.symbols = symbols
+        self.color = color
+
+    def provinces_actions(self, action, province):
+        if action == 'delete':
+            self.provinces.remove(province)
+        if action == 'add':
+            self.provinces.append(province)
+
+    def units_actions(self, action, unit):
+        if action == 'delete':
+            self.units.remove(unit)
+        if action == 'add':
+            self.unita.append(unit)
+
+
+class Unit:
+
+    def create(self, size, mood, id, country_id):
+        self.size = size
+        self.mood = mood
+        self.id = id
+        self.country_id = country_id
+        Country.units_actions("add", id)
+
+
+
 def main():
     pygame.init()  # Инициация PyGame, обязательная строчка
     screen = pygame.display.set_mode(DISPLAY)  # Создаем окошко
     pygame.display.set_caption("Типа стратегия")  # Пишем в шапку
-    bg = Surface((WIN_WIDTH, WIN_HEIGHT))  # Создание видимой поверхности
+    background = Surface((WIN_WIDTH, WIN_HEIGHT))  # Создание видимой поверхности
     # будем использовать как фон
-    bg.fill(Color(BACKGROUND_COLOR))  # Заливаем поверхность сплошным цветом
+    background.fill(Color(BACKGROUND_COLOR))  # Заливаем поверхность сплошным цветом
 
     while 1:  # Основной цикл программы
         for e in pygame.event.get():  # Обрабатываем события
             if e.type == QUIT:
                 raise (SystemExit, "QUIT")
-        screen.blit(bg, (0, 0))  # Каждую итерацию необходимо всё перерисовывать
+        screen.blit(background, (0, 0))  # Каждую итерацию необходимо всё перерисовывать
         pygame.display.update()  # обновление и вывод всех изменений на экран
 
 
